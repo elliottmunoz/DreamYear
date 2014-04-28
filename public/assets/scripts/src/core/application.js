76938550fc7b2fdc096b6a7e68fc967913986fd8
Elliott Munoz <elliottmunoz@mac.com>
2014-04-27 22:48:43 -0400

    Updated for Owl Carousel

--- public/assets/scripts/src/core/application.js
+++ public/assets/scripts/src/core/application.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 	$(".video").fitVids();
-	$(".owl-carousel").owlCarousel();
+	$(".owl-carousel").owlCarousel({
+      items : 5, //10 items above 1000px browser width
+      itemsDesktop : [1000,5], //5 items between 1000px and 961px
+      itemsDesktopSmall : [900,4], // betweem 960p and 601px
+      itemsTablet: [600,2], //2 items between 600 and 0
+      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+  });
 });
 
 
