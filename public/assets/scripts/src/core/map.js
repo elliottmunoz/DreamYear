19092ae17a124356648f75a62b6cf8c931190868
Elliott Munoz <elliottmunoz@mac.com>
2014-05-03 08:51:13 -0400

    Template Updates

---
+++ public/assets/scripts/src/core/map.js
@@ -0,0 +1,38 @@
+/*
+var map = L.mapbox.map('map', 'elliottmunoz.hj6m67pi', {zoomControl: false})
+.setView([39.555, -96.768], 5);
+
+//Basic responsive rules
+function respond(x) {
+
+	if (x >= 640) {
+		map.setZoom(5);
+	}
+
+	if (x < 640) {
+		map.setZoom(4);
+	}
+
+	if (x < 320) {
+		map.setZoom(3);
+	}
+
+}
+
+map.on('resize', function () {
+	var dimensions = map.getSize();
+	var width = dimensions.x;
+
+	respond(width);
+});
+
+map.markerLayer.on('layeradd', function (e) {
+	var marker = e.layer,
+	feature = marker.feature;
+	marker.setIcon(L.icon(feature.properties.icon));
+});
+
+
+map.markerLayer.setGeoJSON(geoJson);
+map.setZoomRange(5, 17);
+*/
